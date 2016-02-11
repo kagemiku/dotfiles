fa57e698b5778ebc9f148bc7618a9d53b2128a82
kage-mac <kagemiku@gmail.com>
2016-02-11 14:53:49 +0900

    Add git setting files.

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,4 +4,6 @@ ln -sf ~/dotfiles/.xvimrc ~/.xvimrc
 ln -sf ~/dotfiles/.vim/colors ~/.vim/colors
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.latexmkrc ~/.latexmkrc
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
 
