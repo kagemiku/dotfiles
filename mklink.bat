6699eab33babeaba5effb7a8132fdcb1f0d5d7a4
KAGE <kagemiku@gmail.com>
2016-04-08 23:24:02 +0900

    Delete mklink.bat file.

--- mklink.bat
+++
@@ -1,6 +0,0 @@
-mklink %HOMEPATH%"\.vimrc" %HOMEPATH%"\dotfiles\.vimrc"
-mklink /D %HOMEPATH%"\.vimrc\colors" %HOMEPATH%"\dotfiles\.vimrc\colors"
-mklink %HOMEPATH%"\.latexmkrc" %HOMEPATH%"\dotfiles\.latexmkrc"
-mklink %HOMEPATH%"\.gitconfig" %HOMEPATH%"\dotfiles\.gitconfig"
-mklink %HOMEPATH%"\.gitignore_global" %HOMEPATH%"\dotfiles\.gitignore_global"
-
